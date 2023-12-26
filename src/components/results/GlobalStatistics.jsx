704396a445690f44569dd0f4573e4483ce3c9558
Anton Arnaudov <70327277+antonarnaudov@users.noreply.github.com>
2023-12-27 00:16:30 +0200

    12 improve tables design and functionality in results page (#16)
    * 12 Rework Global Statistics component to be Table, and added headers to both tables
    
    * 12 Rework the whole Results screen and Statistics components to better visualise data
    
    ---------
    
    Co-authored-by: Anton Arnaudov <anton.arnaudov@audienceplatform.tv>

--- src/components/results/GlobalStatistics.jsx
+++ src/components/results/GlobalStatistics.jsx
@@ -1,143 +1,34 @@
 import React from "react";
 
-import {Stack} from "@mui/material";
-
-import {DefaultTextField} from "../design/TextFeld";
-import {LevInputAdornment} from "../design/InputAdornment";
-
 import {getGlobalStatistics} from "../../data/local-storage";
-
+import TableRow from "@mui/material/TableRow";
+import TableCell from "@mui/material/TableCell";
 
 export const GlobalStatistics = () => {
     const globalStatistics = getGlobalStatistics()
 
     return (
-        <div className="global-statistics">
-            {/*Global Results*/}
-            <Stack
-                id='global-statistics-box'
-                className="global-statistics-box"
-                direction="row"
-                justifyContent="space-evenly"
-                alignItems="center"
-                spacing={2}
-            >
-                {/* Global Info */}
-                <Stack direction="column" spacing={5}>
-                    {/* Shop Name */}
-                    <DefaultTextField
-                        disabled
-                        className="global-name"
-                        defaultValue='Global Statistics'
-                    ></DefaultTextField>
+        <React.Fragment>
+            <TableRow className='global-statistics'>
+                {/*                                Info                                */}
+                <TableCell className='table-cell' >{globalStatistics.total_shops_count}</TableCell>
+                <TableCell className='table-cell'>{globalStatistics.total_products_count}</TableCell>
 
-                    {/* Shops Count */}
-                    <DefaultTextField
-                        disabled
-                        id="global-shops-count"
-                        label="Total Shops Count"
-                        defaultValue={globalStatistics.total_shops_count}
-                    ></DefaultTextField>
+                {/*                         Commission Breakdown                       */}
+                <TableCell className='border-left-dark table-cell'>{globalStatistics.total_products_commissioned}</TableCell>
+                <TableCell className='table-cell'>{globalStatistics.total_pure_commission} лв</TableCell>
+                <TableCell className='table-cell'>{globalStatistics.total_min_profit} лв</TableCell>
 
-                    {/* Products Count */}
-                    <DefaultTextField
-                        disabled
-                        id="global-products-count"
-                        label="Total Products Count"
-                        defaultValue={globalStatistics.total_products_count}
-                    ></DefaultTextField>
-                </Stack>
+                {/*                            Average Values                          */}
+                <TableCell className='border-left-dark table-cell'>{globalStatistics.avg_commission} лв</TableCell>
+                <TableCell className='table-cell'>{globalStatistics.avg_discount} лв</TableCell>
+                <TableCell className='table-cell'>{globalStatistics.avg_shop_profit} лв</TableCell>
 
-                {/* Total Values */}
-                <Stack direction="column" spacing={5}>
-                    {/* Total Commission */}
-                    <DefaultTextField
-                        disabled
-                        id="global-total-commission"
-                        label="Total Commission"
-                        defaultValue={globalStatistics.total_commission}
-                        InputProps={{startAdornment: <LevInputAdornment/>}}
-                    ></DefaultTextField>
-
-                    {/* Total Commission */}
-                    <DefaultTextField
-                        disabled
-                        id="global-total-discount"
-                        label="Total Discount"
-                        defaultValue={globalStatistics.total_discount}
-                        InputProps={{startAdornment: <LevInputAdornment/>}}
-                    ></DefaultTextField>
-
-                    {/* Total Commission */}
-                    <DefaultTextField
-                        disabled
-                        id="global-total-shops-profit"
-                        label="Total Shops Profit"
-                        defaultValue={globalStatistics.total_shop_profit}
-                        InputProps={{startAdornment: <LevInputAdornment/>}}
-                    ></DefaultTextField>
-                </Stack>
-
-                {/* Average Values */}
-                <Stack direction="column" spacing={5}>
-                    {/* Avg Commission */}
-                    <DefaultTextField
-                        disabled
-                        id="global-avg-commission"
-                        label="Average Commission"
-                        defaultValue={globalStatistics.avg_commission}
-                        InputProps={{startAdornment: <LevInputAdornment/>}}
-                    ></DefaultTextField>
-
-                    {/* Avg Commission */}
-                    <DefaultTextField
-                        disabled
-                        id="global-avg-discount"
-                        label="Average Discount"
-                        defaultValue={globalStatistics.avg_discount}
-                        InputProps={{startAdornment: <LevInputAdornment/>}}
-                    ></DefaultTextField>
-
-                    {/* Avg Commission */}
-                    <DefaultTextField
-                        disabled
-                        id="global-avg-shops-profit"
-                        label="Average Shops Profit"
-                        defaultValue={globalStatistics.avg_shop_profit}
-                        InputProps={{startAdornment: <LevInputAdornment/>}}
-                    ></DefaultTextField>
-                </Stack>
-
-                {/* Commission Breakdown */}
-                <Stack direction="column" spacing={5}>
-                    {/* Products Commissioned */}
-                    <DefaultTextField
-                        disabled
-                        id="global-products-commissioned"
-                        label="Total Products Commissioned"
-                        defaultValue={globalStatistics.total_products_commissioned}
-                        InputProps={{startAdornment: <LevInputAdornment/>}}
-                    ></DefaultTextField>
-
-                    {/* Pure Commission */}
-                    <DefaultTextField
-                        disabled
-                        id="global-pure-commission"
-                        label="Total Pure Commission"
-                        defaultValue={globalStatistics.total_pure_commission}
-                        InputProps={{startAdornment: <LevInputAdornment/>}}
-                    ></DefaultTextField>
-
-                    {/* Min Profit */}
-                    <DefaultTextField
-                        disabled
-                        id="global-min-profit"
-                        label="Total Min Profit"
-                        defaultValue={globalStatistics.total_min_profit}
-                        InputProps={{startAdornment: <LevInputAdornment/>}}
-                    ></DefaultTextField>
-                </Stack>
-            </Stack>
-        </div>
+                {/*                             Total Values                           */}
+                <TableCell className='border-left-dark table-cell'>{globalStatistics.total_commission} лв</TableCell>
+                <TableCell className='table-cell'>{globalStatistics.total_discount} лв</TableCell>
+                <TableCell className='table-cell'>{globalStatistics.total_shop_profit} лв</TableCell>
+            </TableRow>
+        </React.Fragment>
     );
 };
--- src/components/results/ResultsFrame.jsx
+++ src/components/results/ResultsFrame.jsx
@@ -15,47 +15,100 @@ import {GlobalStatistics} from "./GlobalStatistics";
 export const ResultsFrame = () => {
     return (
         <div className="results-frame">
-            <GlobalStatistics></GlobalStatistics>
 
-            <TableContainer sx={{width: '90%'}} className='shops-table'>
-                <Table aria-label="collapsible table">
-                    <TableHead>
-                        <TableRow>
-                            <TableCell align="center" colSpan={7}>
+            {/*---------------- Global Breakdown ----------------*/}
+            <TableContainer sx={{width: '95%'}} className='global-statistics-table'>
+                <h1 id='title'>Global Statistics</h1>
+
+                <Table className='table'>
+                    <TableHead className='table-head'>
+                        <TableRow className='table-row'>
+                            <TableCell className='info-cell' align="center" colSpan={2}>
                                 Info
                             </TableCell>
-                            <TableCell className='border-left' align="center" colSpan={3}>
-                                Total Values
+
+                            <TableCell className='breakdown-cell border-left-dark' align="center" colSpan={3}>
+                                Commission Breakdown
                             </TableCell>
-                            <TableCell className='border-left' align="center" colSpan={3}>
+
+                            <TableCell className='avg-cell border-left-dark' align="center" colSpan={3}>
                                 Average Values
                             </TableCell>
-                            <TableCell className='border-left' align="center" colSpan={3}>
+
+                            <TableCell className='total-cell border-left-dark' align="center" colSpan={3}>
+                                Total Values
+                            </TableCell>
+                        </TableRow>
+
+                        <TableRow>
+                            <TableCell className='info-cell-lighter border-left'>Total Shops Count</TableCell>
+                            <TableCell className='info-cell-lighter border-left'>Total Products Count</TableCell>
+
+                            <TableCell className='breakdown-cell-lighter border-left-dark'>Products Commissioned</TableCell>
+                            <TableCell className='breakdown-cell-lighter border-left'>Pure Commission</TableCell>
+                            <TableCell className='breakdown-cell-lighter border-left border-left'>Min Profit</TableCell>
+
+                            <TableCell className='avg-cell-lighter border-left-dark'>Avg Commission</TableCell>
+                            <TableCell className='avg-cell-lighter border-left'>Avg Discount</TableCell>
+                            <TableCell className='avg-cell-lighter border-left'>Avg Shop Profit</TableCell>
+
+                            <TableCell className='total-cell-lighter border-left-dark'>Total Commission</TableCell>
+                            <TableCell className='total-cell-lighter border-left'>Total Discount</TableCell>
+                            <TableCell className='total-cell-lighter border-left'>Total Shop Profit</TableCell>
+                        </TableRow>
+                    </TableHead>
+
+                    <TableBody>
+                        <GlobalStatistics></GlobalStatistics>
+                    </TableBody>
+                </Table>
+            </TableContainer>
+
+
+            {/*---------------- Statistics Breakdown ----------------*/}
+            <TableContainer sx={{width: '95%'}} className='shops-table'>
+                <h1 id='title'>Shops Statistics Breakdown</h1>
+
+                <Table className='table' aria-label="collapsible table">
+                    <TableHead className='table-head'>
+                        <TableRow>
+                            <TableCell className='info-cell' align="center" colSpan={7}>
+                                Info
+                            </TableCell>
+                            <TableCell className='breakdown-cell border-left-dark' align="center" colSpan={3}>
                                 Commission Breakdown
+                            </TableCell>
+                            <TableCell className='avg-cell border-left-dark' align="center" colSpan={3}>
+                                Average Values
+                            </TableCell>
+                            <TableCell className='total-cell border-left-dark' align="center" colSpan={3}>
+                                Total Values
                             </TableCell>
                         </TableRow>
                         <TableRow>
-                            <TableCell/>
-                            <TableCell>Shop Name</TableCell>
-                            <TableCell>Products Count</TableCell>
-                            <TableCell>Commission</TableCell>
-                            <TableCell>Discount</TableCell>
-                            <TableCell>Min Profit</TableCell>
-                            <TableCell>Profit Limit</TableCell>
+                            <TableCell className='info-cell-lighter'/>
 
-                            <TableCell className='border-left'>Total COMM</TableCell>
-                            <TableCell>Total Discount</TableCell>
-                            <TableCell>Total Shop Profit</TableCell>
+                            <TableCell className='info-cell-lighter'>Shop Name</TableCell>
+                            <TableCell className='info-cell-lighter border-left'>Products Count</TableCell>
+                            <TableCell className='info-cell-lighter border-left'>Commission</TableCell>
+                            <TableCell className='info-cell-lighter border-left'>Discount</TableCell>
+                            <TableCell className='info-cell-lighter border-left'>Min Profit</TableCell>
+                            <TableCell className='info-cell-lighter border-left'>Profit Limit</TableCell>
 
-                            <TableCell className='border-left'>Avg COMM</TableCell>
-                            <TableCell>Avg Discount</TableCell>
-                            <TableCell>Avg Shop Profit</TableCell>
+                            <TableCell className='breakdown-cell-lighter border-left-dark'>Products Commissioned</TableCell>
+                            <TableCell className='breakdown-cell-lighter border-left'>Pure Commission</TableCell>
+                            <TableCell className='breakdown-cell-lighter border-left'>Min Profit</TableCell>
 
-                            <TableCell className='border-left'>Products COMM</TableCell>
-                            <TableCell>Pure COMM</TableCell>
-                            <TableCell>Min Profit</TableCell>
+                            <TableCell className='avg-cell-lighter border-left-dark'>Avg Commission</TableCell>
+                            <TableCell className='avg-cell-lighter border-left'>Avg Discount</TableCell>
+                            <TableCell className='avg-cell-lighter border-left'>Avg Shop Profit</TableCell>
+
+                            <TableCell className='total-cell-lighter border-left-dark'>Total Commission</TableCell>
+                            <TableCell className='total-cell-lighter border-left'>Total Discount</TableCell>
+                            <TableCell className='total-cell-lighter border-left'>Total Shop Profit</TableCell>
                         </TableRow>
                     </TableHead>
+
                     <TableBody>
                         {getShops().map((shop) => (
                             <ShopStatistics
